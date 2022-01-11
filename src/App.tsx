3d3b0d66bbc52e05127af42cf064683b3fe1fafc
Mats01 <matej.butkovic@gmail.com>
2022-01-11 11:37:13 +0100

    version 0.1

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,177 @@
-import React from 'react';
+import React, { FC, useCallback, useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const isAlpha = (ch: string): boolean => {
+  return /^[A-ZŠĐŽČĆ]$/i.test(ch);
+}
+
+
+const styles = {
+  app: {
+    display: 'flex',
+    flexDirection: 'column' as 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 60,
+  },
+  container: {
+    border: '1px solid black',
+    width: 40,
+    height: 50,
+    margin: 5,
+    fontSize: 30,
+  },
+  wrapper: {
+    display: 'flex',
+    flexDirection: 'row' as 'row',
+    margin: 'auto',
+  },
+}
+
 function App() {
+
+  const wordOfTheDay = ['š', 'k', 'o', 'l', 'a'];
+
+  const [word, setWord] = useState<string[]>([]);
+  const [colors, setColors] = useState<string[]>(['white', 'white', 'white', 'white', 'white']);
+  const [previousWords, setPreviousWords] = useState<{ word: string[], colors: string[] }[]>([]);
+
+  // useEffect(() => {
+  //   if (previousWords.length > 5) {
+  //     alert('Ostali ste bez pokušaja, rijec je bila: ' + wordOfTheDay.join(''));
+  //   }
+  // }, [previousWords]);
+
+  const getKey = useCallback(
+    (e: KeyboardEvent) => {
+      if (e.key === 'Backspace') {
+        setWord(word.slice(0, -1));
+      }
+      if (e.key === 'Enter') {
+        checkWord();
+      }
+      else if (isAlpha(e.key)) {
+        if (word.length < 5) {
+          setWord([...word, e.key]);
+        }
+      }
+    }, [word])
+
+  const getWiktionary = async (): Promise<boolean> => {
+    return fetch(`https://en.wiktionary.org/w/api.php?action=query&titles=${word.join("")}&prop=revisions&rvprop=content&format=json&origin=*`)
+      .then(
+        (response) =>
+          response.json()
+      ).then((data) => {
+        let pages = data.query.pages;
+        let firstKey = Object.keys(pages)[0];
+        return firstKey !== '-1';
+      }).catch((error) => {
+        console.log(error);
+        return false;
+      })
+
+  }
+
+  const checkWord = async () => {
+    let isWord = await getWiktionary();
+    if (!isWord) {
+      alert('Nije rijec');
+      return;
+    }
+    let newColors = colors;
+    if (word === wordOfTheDay) {
+      alert('Bravo');
+      setColors(['green', 'green', 'green', 'green', 'green']);
+    } else {
+
+      newColors = colors.map((color, index) => {
+        if (wordOfTheDay[index] === word[index]) {
+          return 'green';
+        } else if (wordOfTheDay.includes(word[index])) {
+          return 'yellow';
+        } else {
+          return 'gray';
+        }
+      })
+
+
+    }
+    setPreviousWords([...previousWords, { word: word, colors: newColors }]);
+    setColors(['white', 'white', 'white', 'white', 'white']);
+    setWord([]);
+
+  }
+
+  useEffect(() => {
+    document.addEventListener('keydown', getKey);
+    return () => {
+      document.removeEventListener('keydown', getKey);
+    }
+
+  }, [word])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="App" style={styles.app}>
+      <h1>Rijecle</h1>
+      {previousWords.map((guess) => (
+        <Guesses word={guess.word} colors={guess.colors} />
+      ))}
+      <Guesses word={word} colors={colors} />
+    </div >
   );
 }
 
 export default App;
+
+
+const Guesses: FC<{ word: string[], colors: string[] }> = ({ word, colors }) => {
+
+  return (
+    <div
+      style={styles.wrapper}
+    >
+      <div
+        style={{
+          ...styles.container,
+          backgroundColor: colors[0]
+        }}
+      >
+        {word[0]}
+      </div>
+      <div
+        style={{
+          ...styles.container,
+          backgroundColor: colors[1]
+        }}
+      >
+        {word[1]}
+      </div>
+      <div
+        style={{
+          ...styles.container,
+          backgroundColor: colors[2]
+        }}
+      >
+        {word[2]}
+      </div>
+      <div
+        style={{
+          ...styles.container,
+          backgroundColor: colors[3]
+        }}
+      >
+        {word[3]}
+      </div>
+      <div
+        style={{
+          ...styles.container,
+          backgroundColor: colors[4]
+        }}
+      >
+        {word[4]}
+      </div>
+    </div>
+  )
+}
---
+++ src/rijeci.py
@@ -0,0 +1,344 @@
+
+advenat
+advent
+adventistički
+adventski
+ako
+aktualan
+aktuelan
+akuzativ
+Alaska
+Aljaska
+aljaski
+alnaser
+amaterka
+androgen
+androgin
+anlaser
+anonim
+anonimac
+anonimus
+Apač
+Apači
+Apačica
+apački
+Apaš
+Apaši
+Apašica
+apaški
+apostrof
+auto
+autocesta
+automobil
+autoput
+bančin
+Banjalučanin
+Banjalučanka
+banjalučki
+Banjaluka
+Banjolučanin
+Banjolučanka
+banjolučki
+bankin
+Belgijac
+Belgijanac
+Belgijanka
+Belgijka
+bi
+Biblija
+bih
+bijeliti
+bijeljeti
+bismo
+biste
+blagajna
+blagavaona
+blagavaonica
+blagovaona
+blagovaonica
+blending
+bliskoznačnice
+blizanac
+blizanak
+blizanka
+blizankinja
+bliznac
+bliznak
+bliznakinja
+bo
+bog
+bok
+bol
+boo
+bosti
+Božić
+božićni
+božiji
+božji
+bratov
+bratovljev
+
+broj
+brojka
+Bruno
+bubamara
+bude
+budem
+budemo
+budeš
+budete
+budu
+by
+čekaona
+čekaonica
+
+četirista
+četiristo
+četvrtaš
+ćevabdžija
+ćevabdžinica
+ćevapčići
+ćevapi
+chef
+chefica
+Chinglish
+ciferšlus
+ciljan
+ciljani
+ciljni
+cimerica
+čir
+čirevi
+cirka
+čirovi
+čitaona
+čitaonica
+crniti
+crnjeti
+crvenio
+crveniti
+crvenjeo
+crvenjeti
+cunami
+dalji
+daljnji
+dan
+dar
+dat
+datum
+design
+designirati
+Dino
+dinosaur
+dinosaurus
+disimilacija
+disrumpiran
+disrumpirati
+disrupcija
+disruptiran
+disruptirati
+dizajn
+dizajnerica
+dizajnirati
+dječiji
+dječji
+
+dobijati
+dobivati
+
+dobrodošao
+dobrodošla
+dobrodošle
+dobrodošli
+dodan
+dodat
+dole
+doli
+dolje
+
+došašće
+
+drugaš
+
+drva
+drveta
+drvo
+dug
+dugačak
+  > dvije stotine
+dvjesta
+dvjesto
+dvojina
+  > e - adresa
+   > e - mail
+    > e - pismo
+    > e - poruka
+    > e - pošta
+ekstreman
+  > elektronička pošta
+elektronički
+elektronski
+emajl
+erumpiran
+erumpirati
+erupcija
+erupcijski
+eruptiran
+eruptirati
+eruptivan
+esencijalan
+EU
+  > Europska unija
+evolucija
+evoluirati
+evolvirati
+Facebook
+filtar
+filter
+  > fiskalna blagajna
+      > fiskalna kasa
+frizerka
+  > futur drugi
+      > futur II.
+          gdje
+           > genitiv vremena
+               - jeti / " > glagoli na - iti / -jeti
+                > glavni kuhar
+goriti
+gorjeti
+govedski
+goveđi
+grad
+  > Grad Zagreb
+gristi
+grizen
+gruda
+grudanje
+grudast
+grudati
+grudva
+grudvanje
+grudvast
+grudvati
+ha
+hladiti
+hladnjeti
+  > Hrvatski pravopis
+htio
+htjeo
+hvala
+  > hvala lijepa
+      > hvala lijepo
+idealan
+igdje
+ikamo
+ikuda
+imejl
+impressum
+impresum
+Indijanci
+injekcija
+internet
+inženjerka
+  > ispričavam se
+ispričavanje
+  > ispričavati se
+Isprika
+isti
+istoznačnice
+istraživanje
+izbo
+izboo
+izbosti
+izgristi
+izgrizen
+izluditi
+izludjeti
+iznemoći
+izostavnik
+  > izvanredne vijesti
+      > izviniti se
+          > izvinjavati se
+izvinjenje
+jaja
+jaje
+jajeta
+    > je l'< /a > </li >
+
+      kamo li
+
+          kiselo mlijeko
+
+               koristiti
+
+                  lektura
+
+                   mini suknja
+
+                      mocijski parnjaci
+
+                           naseliti
+
+                              nedaleko od
+
+                                  ni...ni
+
+                                      niti...niti
+
+                                          Nova godina
+
+                                               odmarati
+
+                                                   odmoriti
+
+                                                       odseliti
+
+                                                          osnovna škola
+
+                                                              patentni zatvarač
+
+                                                                   plavjeti
+                                                                      pluralia tantum
+
+                                                                          pokvareno mlijeko
+
+                                                                              prerijski psi
+
+                                                                                   preseliti
+
+                                                                                      prometna nesreća
+                                                                                          prometna nezgoda
+
+                                                                                              prvi put
+                                                                                                  prvi puta
+
+                                                                                                       rumenjeti
+
+                                                                                                            /"> s obzirom na < /a > </li >
+s vremenom
+
+  saobraćajna nesreća
+
+       seliti
+
+           sivjeti
+
+              sretan Božić
+                    /"> sretna nova godina < /a > </li >
+
+                    Stari Grad
+
+                      svaki put
+                          svaki puta
+
+                              tri stotine
+
+                                  ukiseljeno mlijeko
+
+
+            zahvaliti
+               zelenjeti
+                   zgrudati
+                       zgrudvati
+                          životinjska komunikacija
+                               žutjeti
